5ebe70007b966bb21ab6319ababb09a108268d39
forthenew <forthenew@gmail.com>
2020-12-16 22:40:31 +0900

    Initial Commit

---
+++ Storyboard_MoviePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storyboard_MoviePlayer
+//
+//  Created by stam on 2020/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
